bc9ad47eb2aa826e3b2dcb2713c5db9e09d5565c
Arnaud Champion <arnaudchampion76@gmail.com>
2025-01-07 00:58:28 +0700

    Enhance styling and structure of login component; update asset paths in angular.json and tailwind.config.js. Added custom styles for buttons, inputs, and dropdowns, and improved overall layout and accessibility features.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,20 @@ module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#61BAAC',
+        secondary: '#807A70',
+        background: '#171821',
+        'card-bg': '#1E1F2B',
+        'text': '#FFFFFF',
+      },
+      borderRadius: {
+        DEFAULT: '4px',
+      },
+    },
   },
   plugins: [],
 }

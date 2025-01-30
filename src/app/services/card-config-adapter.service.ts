9d037a7130f660368ed44d80ced419b327cd5f0a
Arnaud Champion <arnaudchampion76@gmail.com>
2025-01-30 11:13:40 +0700

    I apolog

---
+++ src/app/services/card-config-adapter.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { BaseCardConfig, CardDto } from '@models/api.models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CardConfigAdapterService {
+  toCardDto<T extends BaseCardConfig>(config: T): CardDto<T> {
+    // Configuration is already in the right format as it extends BaseCardConfig
+    // We just need to convert it to CardDto
+    return {
+      id: 0,  // Will be set by backend
+      type: config.constructor.name.toLowerCase().replace('config', ''),
+      title: [],  // Will be filled by component
+      order: 0,  // Managed by layout editor
+      gridWidth: 4,  // Default value
+      backgroundColor: 0x1f2937,  // Default value
+      textColor: 0xffffff,  // Default value
+      headerTextColor: 0xffffff,  // Default value
+      headerBackgroundColor: 0x1f2937,  // Default value
+      rowId: 0,  // Managed by layout editor
+      displayHeader: true,  // Default value
+      displayFooter: false,  // Default value
+      icon: '',  // Will be filled by component
+      configuration: config  // Specific configuration
+    };
+  }
+
+  fromCardDto<T extends BaseCardConfig>(dto: CardDto<T>): T {
+    // Specific configuration is already in the right format
+    // as it extends BaseCardConfig
+    return dto.configuration as T;
+  }
+} 
+
---
+++ src/app/services/card-validation.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { BaseCardConfig } from '@models/api.models';
+import { DatasourceConfig } from '@models/datasource.models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CardValidationService {
+  validateConfig(config: BaseCardConfig): boolean {
+    if (!config) return false;
+
+    // Required fields validation
+    if (!config.toJson) {
+      return false;
+    }
+
+    return true;
+  }
+
+  validateDataConfig(config: DatasourceConfig): boolean {
+    if (!config) return false;
+
+    // Type validation
+    if (!['API', 'EntityFramework', 'SQLQuery'].includes(config.type)) {
+      return false;
+    }
+
+    // Type-specific validation
+    switch (config.type) {
+      case 'API':
+        if (!config.controller) return false;
+        break;
+      case 'EntityFramework':
+        if (!config.entity || !config.connection) return false;
+        break;
+      case 'SQLQuery':
+        if (!config.query || !config.connection) return false;
+        break;
+    }
+
+    // A stored procedure may or may not have parameters
+    // Just check that if parameters exist, they are well-formed
+    if (config.isStoredProcedure && config.procedureParameters) {
+      // Check that each parameter has at least a name and type
+      for (const param of Object.values(config.procedureParameters)) {
+        if (!param.name || !param.type) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+} 
